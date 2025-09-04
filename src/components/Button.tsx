73759cf37a01759e538068ed97a69b1d8687939e
marcos-wesley <marcos.wesley@hotmail.com.br>
2025-09-04 11:35:50 -0300

    App Finalizado

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -35,7 +35,7 @@ export default function Button(props) {
     if(props.operation) stylesButton.push(styles.operationButton)
 
   return (
-    <TouchableHighlight onPress={props.onClick}>
+    <TouchableHighlight onPress={() => props.onClick(props.label)}>
       <Text style={stylesButton}>{props.label}</Text>
     </TouchableHighlight>
   );
